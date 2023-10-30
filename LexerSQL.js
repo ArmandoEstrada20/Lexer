d15930d027faf4e10ca967f3a5e10aa80179bf56
Hector Armando Ramirez Estrada <137834404+ArmandoEstrada20@users.noreply.github.com>
2023-10-30 23:12:49 +0000

    Correción del código para la asignación de token acorde a la palabra reservada contenida.

--- LexerSQL.js
+++ LexerSQL.js
@@ -1,33 +1,58 @@
 /*Realizar un programa que analice un achivo, el cual contendrá sentencias SQL.
-El programa leerá las sentencias y las mostrará separadas y en posición vertical,
-respetando la sintaxis de SQL, incluyendo palabras reservadas, operadores, comillas, etc.
+El programa leerá las sentencias y las mostrará en pantalla, al mismo tiempo que leerá el archivo sqlkeywords
+y segun las sentencias que esten contenidas en el archivo sql se le asignará un token a cada palabra de dicho archivo.
 */
-
 const fs = require('fs');
-const archivo = 'sql.txt';
 
-//Primero verificaremos que el archivo se encuentre creado
-if(!fs.existsSync(archivo)){
-    //Si no existe, se creeará el archivo con el nombre indicado.
-    fs.writeFileSync(archivo,'','utf8');
-    console.log(`El archivo ${filename} ha sido creado.`)
-}
+const archivoSQL = 'sql.txt';
+const archivoKeywords = 'sqlkeywords.txt';
 
-fs.readFile('sql.txt', 'utf8', (err, data) => {
-    if(err){
+// Crear un mapa para almacenar las palabras clave y sus números asociados
+const keywordsMap = new Map();
+
+// Leer el archivo de palabras clave y cargarlo en el mapa
+fs.readFile(archivoKeywords, 'utf8', (err, data) => {
+    if (err) {
         console.error(err);
         return;
     }
+    const lines = data.split('\n');
+    lines.forEach((line) => {
+        const [num, keyword] = line.split(' ');
+        keywordsMap.set(keyword, num);
+    });
 
-    var divString = data.split('\n');
-    const cadena = divString[0].split('\n');
-    //Ciclo Principal
-    for(let p = 0; p < divString.length; p++){
-        var query = divString[p].split(" ");
-        console.log("-------------------------" + '\n');
-        console.log("Renglon " + p + " : ");
-        for(let r = 0; r < query.length; r++){
-            console.log(query[r] + '\n');
+    // Leer el archivo SQL y tokenizar las sentencias
+    fs.readFile(archivoSQL, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
         }
-    }
+        const sentencias = data.split('\n');
+
+        // Tokenizar y asignar números
+        sentencias.forEach((sentencia, index) => {
+            const tokens = sentencia.split(/\s+/); // Dividir por espacios en blanco
+            const numerosTokens = tokens.map((token) => {
+                let numero;
+                if(!isNaN(token)){
+                    numero = 995;
+                } else {
+                    numero = keywordsMap.get(token.toUpperCase());
+                    if(numero === undefined){
+                        numero = 999;
+                    }
+                }
+                return numero;
+                /*const numero = keywordsMap.get(token.toUpperCase());
+                return numero !== undefined ? numero : token;*/
+            });
+
+            console.log(`Sentencia ${index + 1}:`);
+            console.log(sentencia);
+            console.log('Token:');
+            console.log(numerosTokens.join('|'));
+            console.log('-------------------------');
+        });
+    });
 });
